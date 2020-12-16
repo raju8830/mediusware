7f01cd672a736a4549331ff9f356c375731fb2ce
raju8830 <44889262+raju8830@users.noreply.github.com>
2020-12-16 23:55:38 +0600

    Mediures
    Mediures

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('#contact_jquery').click(function(){
+		$('#hide_part').hide(1000);
+	});
+	//mixitup portfolio section
+	var mixer = mixitup('.mix_slider_part');
+});
